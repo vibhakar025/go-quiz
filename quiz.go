19d48080ee0c4951096bfc7589eb854e451df437
vibhakar025 <vibhakar025@gmail.com>
2023-07-07 23:06:58 +0530

    added goroutine to run timer for quiz

--- quiz.go
+++ quiz.go
@@ -5,17 +5,22 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
+	"time"
 )
 
 func main() {
 
 	filename := flag.String("csv", "problems.csv", "csv file in question, answer format")
+	timelimit := flag.Int("limit", 30, "time limit in seconds")
 	flag.Parse()
 
 	file, err := os.Open(*filename)
 	if err != nil {
 		exit(fmt.Sprintf("Failed to open csv file %s", *filename))
 	}
+
+	defer file.Close()
 
 	reader := csv.NewReader(file)
 
@@ -25,8 +30,34 @@ func main() {
 	}
 
 	problems := parseData(filedata)
-	fmt.Println(problems)
 
+	timer := time.NewTimer(time.Duration(*timelimit) * time.Second)
+
+	score := 0
+	answerCh := make(chan string)
+
+	for _, problem := range problems {
+		fmt.Printf("%s = ", problem.q)
+
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Printf("\nYou scored %d out of %d\n", score, len(problems))
+			return
+
+		case answer := <-answerCh:
+			if answer == problem.a {
+				fmt.Println("Correct!")
+				score++
+			}
+		}
+	}
+	fmt.Printf("\nYou scored %d out of %d\n", score, len(problems))
 }
 
 type Problem struct {
@@ -39,7 +70,7 @@ func parseData(filedata [][]string) []Problem {
 	for _, record := range filedata {
 		problem := Problem{
 			q: record[0],
-			a: record[1],
+			a: strings.TrimSpace(record[1]),
 		}
 		problems = append(problems, problem)
 	}
